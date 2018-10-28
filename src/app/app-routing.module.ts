ef19e4ee5dd3711cc430b9f65be1f17a441af97b
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-28 18:50:31 +0530

    routing configured for chartboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ChartboardComponent } from "./chartboard/chartboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/chartboard", pathMatch: "full"},
+  { path: "chartboard", component: ChartboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
