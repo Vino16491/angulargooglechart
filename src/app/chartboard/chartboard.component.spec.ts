6bb5ca15a4db23d2b7de540ad831b884b1c0f5f8
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-28 18:41:57 +0530

    new component chartboard created

---
+++ src/app/chartboard/chartboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ChartboardComponent } from './chartboard.component';
+
+describe('ChartboardComponent', () => {
+  let component: ChartboardComponent;
+  let fixture: ComponentFixture<ChartboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ChartboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ChartboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
