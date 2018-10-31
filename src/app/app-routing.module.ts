da9d934270023b8016dd9534d9535cd5dfdd31d0
vinod g chandaliya <vinodgchandaliya@gmail.com>
2018-10-31 20:00:57 +0530

    navigation bar added for chart routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ChartboardComponent } from "./chartboard/chartboard.component";
+import { ChartJsComponent } from "./chart-js/chart-js.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/chartboard", pathMatch: "full"},
-  { path: "chartboard", component: ChartboardComponent }
+  { path: "", redirectTo: "/chartboard", pathMatch: "full" },
+  { path: "chartboard", component: ChartboardComponent },
+  { path: "chartjs", component: ChartJsComponent }
 ];
 
 @NgModule({
